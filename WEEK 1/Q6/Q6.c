059ca07190027b3103a5964002ecdbb623ae0185
TanmayTiwari-1 <premt344@gmail.com>
2026-08-02 16:24:32 +0530

    Implement duplicate check and comparisons logging
    This program checks for duplicates in an array and logs the number of comparisons made while searching for duplicates. It generates a CSV file with the results for different array sizes.

---
+++ WEEK 1/Q6/Q6.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+long long comparisons;
+
+
+int hasDuplicate(int arr[], int n)
+{
+    comparisons = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            comparisons++;
+
+            if (arr[i] == arr[j])
+                return 1;
+        }
+    }
+
+    return 0;
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    FILE *fp = fopen("comparisons.csv", "w");
+
+    if (fp == NULL)
+    {
+        printf("Error creating CSV file!\n");
+        return 1;
+    }
+
+    fprintf(fp, "Array Size,Comparisons,Duplicate Found\n");
+
+    for (int n = 100; n <= 5000; n += 100)
+    {
+        int arr[n];
+
+     
+        for (int i = 0; i < n; i++)
+            arr[i] = rand() % 1000000;
+
+        int duplicate = hasDuplicate(arr, n);
+
+        fprintf(fp, "%d,%lld,%s\n",
+                n,
+                comparisons,
+                duplicate ? "Yes" : "No");
+
+        printf("n = %4d   Comparisons = %-10lld   Duplicate = %s\n",
+               n,
+               comparisons,
+               duplicate ? "Yes" : "No");
+    }
+
+    fclose(fp);
+
+    printf("\ncomparisons.csv generated successfully.\n");
+
+    return 0;
+}
